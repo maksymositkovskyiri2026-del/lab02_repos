568855eace997759abe970d5fbf07fcc3caae6c3
maksymositkovskyiri2026-del <maksym.ositkovskyi.ri.2026@lpnu.ua>
2026-09-23 13:41:17 +0300

    change

--- Lb2.1/Lb2.1.cpp
+++ Lb2.1/Lb2.1.cpp
@@ -20,11 +20,11 @@ int main()
 
 	// Виправлено групування дужок для чисельника (1 + a + a^2)
 	z1 = pow(((1 + a + pow(a, 2)) / (2 * a + pow(a, 2))) + 2 - ((1 - a + pow(a, 2)) / (2 * a - pow(a, 2))), -1) * (5 - 2 * pow(a, 2));
-	//z2 = (4 - pow(a, 2)) / 2.0;
+	z2 = (4 - pow(a, 2)) / 2.0;
 
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
